ddaceab38dfbb29e1ff85ffa43d9249ed4179e85
Denis Ashcheulov <dashcheulov@iponweb.net>
2018-01-05 05:29:27 +0300

    made Utilities serializable

--- src/net/iponweb/Utilities.groovy
+++ src/net/iponweb/Utilities.groovy
@@ -1,30 +1,32 @@
 #!/usr/bin/env groovy
 package net.iponweb
-class Utilities {
-  static def notifyBuild(script, String buildStatus) {
+class Utilities implements Serializable {
+  def g
+  Utilities(script) {this.g = script}
+  def notifyBuild(currentBuild, String buildStatus) {
     buildStatus =  buildStatus ?: 'SUCCESS'
-    def subject = "${script.env.JOB_NAME} - #${script.env.BUILD_NUMBER} ${buildStatus}"
+    def subject = "${g.env.JOB_NAME} - #${g.env.BUILD_NUMBER} ${buildStatus}"
     def colorCode = '#00AA00'
     def changeLogMsg = 'Changes:\n'
-    if (script.currentBuild.changeSets.size() >= 1) {
-        def entries = script.currentBuild.changeSets[0].items
+    if (currentBuild.changeSets.size() >= 1) {
+        def entries = currentBuild.changeSets[0].items
         for (int j = 0; j < entries.length; j++) {
             changeLogMsg = changeLogMsg + "- ${entries[j].msg} [${entries[j].author}]\n"
         }
     } else {
         changeLogMsg = 'No changes\n'
     }
-    long bt = (System.currentTimeMillis() - script.currentBuild.startTimeInMillis) / 1000
+    long bt = (System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000
     long btm = bt/60
     long bts = bt%60
     def btime = String.format("%d min %02d sec", btm, bts)
-    def summary = "${subject} after ${btime} (<${script.env.BUILD_URL}|Open>)\n${changeLogMsg}"
+    def summary = "${subject} after ${btime} (<${g.env.BUILD_URL}|Open>)\n${changeLogMsg}"
     if (buildStatus == 'STARTED') {
       colorCode = '#EEEE00'
-      summary = "${subject} (<${script.env.BUILD_URL}|Open>)"
+      summary = "${subject} (<${g.env.BUILD_URL}|Open>)"
     } else if (buildStatus == 'FAILURE') {
       colorCode = '#AA0000'
     }
-    script.slackSend (color: colorCode, message: summary)
+    g.slackSend (color: colorCode, message: summary)
   }
 }
